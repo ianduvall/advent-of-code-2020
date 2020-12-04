b06a2693ee477d63aaea73208208ccb28c71458d
Ian Duvall <iduvall@umich.edu>
2020-12-04 14:22:06 -0600

    Add prettier

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const readFileToArray = require('../readFileToArray');
+const path = require("path");
+const readFileToArray = require("../readFileToArray");
 
 const main = async () => {
-    const expenses = await readFileToArray(path.join(__dirname, '/input.txt'));
+	const expenses = await readFileToArray(path.join(__dirname, "/input.txt"));
 
-    const lookup = expenses.reduce((acc, expense) => {
-        acc[expense] = expense;
-        return acc;
-    }, {});
+	const lookup = expenses.reduce((acc, expense) => {
+		acc[expense] = expense;
+		return acc;
+	}, {});
 
-    let answer = -1;
-    for (let expense of expenses) {
-        let otherExpense = lookup[2020 - expense];
-        if (otherExpense) {
-            answer = expense * otherExpense;
-            break;
-        }
-    }
+	let answer = -1;
+	for (let expense of expenses) {
+		let otherExpense = lookup[2020 - expense];
+		if (otherExpense) {
+			answer = expense * otherExpense;
+			break;
+		}
+	}
 
-    console.log(answer);
+	console.log(answer);
 };
 
 main();
