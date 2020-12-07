8478d249c59ed32fe39b2647b94111552f66b2d0
Ian Duvall <iduvall@umich.edu>
2020-12-07 14:37:52 -0600

    Day 2 part 2

--- src/day2/index.js
+++ src/day2/index.js
@@ -4,24 +4,38 @@ const readFileToArray = require("../readFileToArray");
 const main = async () => {
 	const rows = await readFileToArray(path.join(__dirname, "/input.txt"));
 
-	const answer = rows.reduce((numValidPasswords, row, i) => {
-		const [policy, password] = row.split(": ");
-		const [rangeString, policyChar] = policy.split(" ");
-		const range = rangeString.split("-");
-		const min = parseInt(range[0], 10);
-		const max = parseInt(range[1], 10);
+	const { part1Answer, part2Answer } = rows.reduce(
+		(acc, row, i) => {
+			const [policy, password] = row.split(": ");
+			const [rangeString, policyChar] = policy.split(" ");
+			const range = rangeString.split("-");
+			const min = parseInt(range[0], 10);
+			const max = parseInt(range[1], 10);
 
-		let numChars = 0;
-		for (let char of password) {
-			if (char === policyChar) ++numChars;
-		}
+			let numChars = 0;
+			for (let char of password) {
+				if (char === policyChar) ++numChars;
+			}
 
-		return numChars >= min && numChars <= max
-			? numValidPasswords + 1
-			: numValidPasswords;
-	}, 0);
+			if (numChars >= min && numChars <= max) ++acc.part1Answer;
 
-	console.log(answer);
+			const minContainsChar = password.charAt(min - 1) === policyChar;
+			const maxContainsChar = password.charAt(max - 1) === policyChar;
+			if (
+				(minContainsChar || maxContainsChar) &&
+				!(minContainsChar && maxContainsChar)
+			) {
+				++acc.part2Answer;
+			}
+
+			return acc;
+		},
+		{ part1Answer: 0, part2Answer: 0 }
+	);
+
+	console.log("Part 1:", part1Answer);
+
+	console.log("Part 2:", part2Answer);
 };
 
 main();
